24a1c69ffaf07a5ba79088cf6e6205ffa42dba29
KhiemNTK <khiem14205@gmail.com>
2026-06-03 01:10:38 +0700

    feat: add string-util

--- src/common/utils/string-util/string-util.service.ts
+++ src/common/utils/string-util/string-util.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import { kebabCase } from 'es-toolkit/compat';
 @Injectable()
-export class StringUtilService {}
+export class StringUtilService {
+  removeSpace(value: string) {
+    return value.replace(/\s+/g, '_');
+  }
+
+  toSlug(text: string): string {
+    return kebabCase(
+      text
+        .normalize('NFD') // split VietNamese characters
+        .replace(/[\u0300-\u036f]/g, '') //delete the split characters
+        .replace(/[đĐ]/g, 'd'), //đ to d
+    );
+  }
+}
